746cc809d94197eca78c9f1e9842cc636d704bc1
gerardo <gerardojriveros@hotmail.com>
2015-03-24 19:10:09 -0400

    Test

--- SGPA/apps/usuario/tests.py
+++ SGPA/apps/usuario/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+import unittest
+
+class SimplisticTest(unittest.TestCase):
+    def test(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
+
+class UseraddTest(unittest.TestCase):
+    def test(self):
+        u = User()
+        name = 'Jose'
+        u.username = 'Jose'
+        u.save
