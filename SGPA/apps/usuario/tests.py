f4abdd53f844195c244cbed558bbbbb3a252ebbe
gerardo <gerardojriveros@hotmail.com>
2015-03-25 13:18:09 -0400

    Testcambios

--- SGPA/apps/usuario/tests.py
+++ SGPA/apps/usuario/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-
+import unittest
 
 class UserTestCase(TestCase):
     def setUp(self):
@@ -9,3 +9,6 @@ class UserTestCase(TestCase):
     def test_user_add(self):
         user = User.objects.get(name="Jose")
         self.assertEqual(user.get_username,'Jose')
+
+if __name__ == "__main__":
+            unittest.main()
