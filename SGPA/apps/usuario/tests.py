b40f0491f62ddd8ddf7bc78d7210e15d6b40c90e
gerardo <gerardojriveros@hotmail.com>
2015-03-24 21:26:22 -0400

    Test2

--- SGPA/apps/usuario/tests.py
+++ SGPA/apps/usuario/tests.py
@@ -1,10 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from django.conf import settings
-import SGPA
 
-if not settings.configured:
-    settings.configure(SGPA.apps, DEBUG=True)
 
 class UserTestCase(TestCase):
     def setUp(self):
