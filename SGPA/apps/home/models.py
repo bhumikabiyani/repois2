f19cd025d46cea73ab4fbd052c4887d5618c8141
jorgemeza <jorgemeza93@gmail.com>
2015-03-25 20:49:25 -0400

    models User

--- SGPA/apps/home/models.py
+++ SGPA/apps/home/models.py
@@ -3,15 +3,16 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class userProfile(models.Model):
+    pass
 	
-	def url(self,filename):
-		ruta = "MultimediaData/Users/%s/%s"%(self.user.username,filename)
-		return ruta
-
-
-	user 		=	models.OneToOneField(User)
-	photo 		=	models.ImageField(upload_to=url)
-	telefono	=	models.CharField(max_length=30)
-
-	def __unicode__(self):
-		return self.user.username
+	# def url(self,filename):
+	# 	ruta = "MultimediaData/Users/%s/%s"%(self.user.username,filename)
+	# 	return ruta
+    #
+    #
+	# user 		=	models.OneToOneField(User)
+	# photo 		=	models.ImageField(upload_to=url)
+	# telefono	=	models.CharField(max_length=30)
+    #
+	# def __unicode__(self):
+	# 	return self.user.username
--- SGPA/apps/usuario/models.py
+++ SGPA/apps/usuario/models.py
@@ -96,216 +96,216 @@ class RolUsuarioProyecto(models.Model):
     #def __unicode__(self):
      #   return self.nombre
 
-class TipoItem(models.Model):
-    """"Esta clase representa a que tipo pertenece un item."""
-    nombre = models.CharField(unique=True, max_length=50)
-    descripcion = models.CharField(max_length=50)
-    #claves foraneas
-    #fase = models.ForeignKey(Fase)
-    
-    def __unicode__(self):
-        return self.nombre
-
-
-
-class Fase(models.Model):
-    """Esta clase representa la fase del proyecto."""
-    nombre = models.CharField(unique=True, max_length=50)
-    descripcion = models.TextField(null=True, blank=True)      
-#    nro_secuencia = models.IntegerField()
-    tipo_item = models.ForeignKey(TipoItem)
-    proyecto= models.IntegerField()
-    estado = models.CharField(max_length=50,choices=FASE_ESTADO)
-    def __unicode__(self):
-        return self.nombre
-
-class TipoItemFase(models.Model):
-    """Tabla que relaciona el tipo de item a una fase por proyecto."""
-    fase = models.ForeignKey(Fase)
-    #fase = models.ForeignKey(Fase)
-    tipo_item = models.ForeignKey(TipoItem)
-    cant = models.IntegerField(null=0)
-
-    def __unicode__(self):
-        return self.tipo_item.descripcion
-
-    class Meta:
-        unique_together = [("tipo_item",  "fase")]
-#class TipoItem(models.Model):
- #   """"Esta clase representa a que tipo pertenece un item."""
-  #  nombre = models.CharField(unique=True, max_length=50)
-   # descripcion = models.CharField(max_length=50)
-    #claves foraneas
-    #fase = models.ForeignKey(Fase)
-
-    #def __unicode__(self):
-     #   return self.nombre
-
-#class TipoItemFaseProyecto(models.Model):
- #   """Tabla que relaciona el tipo de item a una fase por proyecto."""
-  #  proyecto = models.ForeignKey(Proyecto)
-    #fase = models.ForeignKey(Fase)
-   # tipo_item = models.ForeignKey(TipoItem)
-    #cant = models.IntegerField(max_length = 4)
-  
-    #def __unicode__(self):
-     #   return self.tipo_item.descripcion
-
-    #class Meta:
-     #   unique_together = [("tipo_item",  "proyecto")]
-
-class LineaBase(models.Model):
-    #fecha_creacion = models.DateField(auto_now=False, auto_now_add=True, editable=False)
-    #relaciones con otras tablas
-    #proyectos = models.ForeignKey(Proyecto)
-    #fase = models.ForeignKey(Fase)
-     nombre = models.CharField(max_length=50)
-    # items = models.IntegerField()
-     #estado = modesl.CharField(max_length=50)
-     estado = models.CharField(max_length=1, choices=LB_CHOICES)
-     fase = models.ForeignKey(Fase)
-
-
-class Item(models.Model):
-    """Clase que representa a los items."""
-    nombre = models.CharField(max_length=50)
-    usuario = models.ForeignKey(User)
-    fecha_creacion = models.DateField(auto_now_add=True, editable = False)
-    estado = models.IntegerField(max_length=1, choices=STATUS_CHOICES, default=1)    
-    version = models.PositiveIntegerField()
-    complejidad = models.IntegerField(max_length=1, choices=COMPLEXITY_CHOICES)
-    descripcion_corta = models.TextField(null=True, blank=True)
-    descripcion_larga = models.TextField(null=True, blank=True)
-    habilitado = models.BooleanField(default=True)
-    icono = models.FileField(upload_to='icono', null=True, blank=True)
-    #claves foraneas
-    proyecto = models.ForeignKey(Proyecto)
-    tipo = models.ForeignKey(TipoItemFase)
-    fase = models.ForeignKey(Fase)
-    lbase= models.ForeignKey(LineaBase) 
-    def __unicode__(self):
-        return self.nombre
-
-class RelItem(models.Model):
-    padre = models.ForeignKey(Item, related_name = 'padre')
-    hijo = models.ForeignKey(Item, related_name = 'hijo')
-    habilitado = models.BooleanField(default=True)
-    
-    class Meta:
-        unique_together = [("padre", "hijo")]
-
-class Historial(models.Model):
-    """Clase que representa el historial de los items"""
-    usuario = models.ForeignKey(User)
-    fecha_creacion = models.DateField(auto_now =False, auto_now_add=True, editable=False)
-    #claves foraneas
-    item = models.OneToOneField(Item, parent_link=False)
-    
-class RegistroHistorial(models.Model):
-    """Clase que representa el Registro de versiones de los items"""
-    version = models.PositiveIntegerField()
-    complejidad = models.IntegerField()
-    descripcion_corta = models.TextField(null=True, blank=True)
-    descripcion_larga = models.TextField(null=True, blank=True)
-    habilitado = models.BooleanField()
-    icono = models.FileField(upload_to='icono', null=True, blank=True)
-    tipo = models.ForeignKey(TipoItemFase)
-    fecha_modificacion = models.DateTimeField(auto_now=True, auto_now_add=False, editable=False)
-    #claves foraneas
-    historial = models.ForeignKey(Historial)
-   
-    
-class Adjunto(models.Model):
-    #archivo = models.FileField(upload_to='items')
-    nombre = models.CharField(max_length = 100)
-    contenido = models.TextField(null=True)
-    tamano = models.IntegerField()
-    mimetype = models.CharField(max_length = 255)  
-    #claves foraneas
-    item = models.ForeignKey(Item)
-    habilitado = models.BooleanField(default = True)
-LB_CHOICES = ( ('R','REVISADO'),
-	       ('NR','NO REVISADO'),
-		('L','LIBERADO'),
-	)
-#class LineaBase(models.Model):
-    #fecha_creacion = models.DateField(auto_now=False, auto_now_add=True, editable=False)
-    #relaciones con otras tablas
-    #proyectos = models.ForeignKey(Proyecto)
-    #fase = models.ForeignKey(Fase)
- #    nombre = models.CharField(max_length=50)
-    # items = models.IntegerField()
-     #estado = modesl.CharField(max_length=50)
-  #   estado = models.CharField(max_length=1, choices=LB_CHOICES)
-   #  fase = models.ForeignKey(Fase)     
-class UsuarioRolProyecto(models.Model):   
-    usuario = models.ForeignKey(User)
-    rol = models.ForeignKey(Rol, null=True)
-    proyecto = models.ForeignKey(Proyecto)
-
-    class Meta:
-        unique_together = [("usuario", "rol", "proyecto")]
-        
-class UsuarioRolSistema(models.Model):
-    usuario = models.ForeignKey(User)
-    rol = models.ForeignKey(Rol)
-    
-    class Meta:
-        unique_together = [("usuario", "rol")]
-        
-class RegHistoRel(models.Model):
-    itm_padre = models.ForeignKey(Item, related_name = 'itm_padre')
-    itm_hijo = models.ForeignKey(Item, related_name = 'itm_hijo')
-    registro = models.ForeignKey(RegistroHistorial)
-    
-    class Meta:
-        unique_together = [("itm_padre", "itm_hijo", "registro")]
-        
-class RegHistoAdj(models.Model):
-    #archivo = models.FileField(upload_to='items')
-    nombre = models.CharField(max_length = 100)
-    contenido = models.TextField(null=True)
-    tamano = models.IntegerField()
-    mimetype = models.CharField(max_length = 255)  
-    #claves foraneas
-    item = models.ForeignKey(Item)
-    #habilitado = models.BooleanField(default = True)
-    registro = models.ForeignKey(RegistroHistorial)
-
-
-class LineaBaseItems(models.Model):
-    linea = models.ForeignKey(LineaBase)
-    items = models.ForeignKey(Item)
-    #procesos = models.CharField(max_length=1, choices=ACCIONES_CHOICES)
-
-    class Meta:
-        unique_together = [("linea", "items")]
-
-
-ESTADO_SOLICITUD = (
-                 ('A', 'Aprobado'),
-                 ('P', 'Pendiente'),
-		 ('R', 'Rechazado'),
-             )
-
-class Comite(models.Model):
-	usuarios = models.ForeignKey(User)
-        proyecto= models.ForeignKey(Proyecto)
-
-class Solicitud(models.Model):
-	usuario_solicitante = models.ForeignKey(User)
-	estado = models.CharField(max_length=1, choices=ESTADO_SOLICITUD)
-
-	def __unicode__(self):
-		return self.usuario_solicitante
-ACCIONES_CHOICES = ( ('M', 'Modificar'),
-		     ('B', 'Borrar'),)
-class Acciones(models.Model):
-    procesos = models.CharField(max_length=1, choices=ACCIONES_CHOICES)
-    items = models.ForeignKey(Item)
-    usu_solicitante = models.ForeignKey(User)
-    proyecto =models.ForeignKey(Proyecto)
-    aceptado = models.IntegerField(default=0)	
-    rechazado = models.IntegerField(default=0)
-    votante= models.IntegerField()
-    estado = models.CharField(max_length=1, choices=ESTADO_SOLICITUD)
+# class TipoItem(models.Model):
+#     """"Esta clase representa a que tipo pertenece un item."""
+#     nombre = models.CharField(unique=True, max_length=50)
+#     descripcion = models.CharField(max_length=50)
+#     #claves foraneas
+#     #fase = models.ForeignKey(Fase)
+#
+#     def __unicode__(self):
+#         return self.nombre
+#
+#
+#
+# class Fase(models.Model):
+#     """Esta clase representa la fase del proyecto."""
+#     nombre = models.CharField(unique=True, max_length=50)
+#     descripcion = models.TextField(null=True, blank=True)
+# #    nro_secuencia = models.IntegerField()
+#     tipo_item = models.ForeignKey(TipoItem)
+#     proyecto= models.IntegerField()
+#     estado = models.CharField(max_length=50,choices=FASE_ESTADO)
+#     def __unicode__(self):
+#         return self.nombre
+#
+# class TipoItemFase(models.Model):
+#     """Tabla que relaciona el tipo de item a una fase por proyecto."""
+#     fase = models.ForeignKey(Fase)
+#     #fase = models.ForeignKey(Fase)
+#     tipo_item = models.ForeignKey(TipoItem)
+#     cant = models.IntegerField(null=0)
+#
+#     def __unicode__(self):
+#         return self.tipo_item.descripcion
+#
+#     class Meta:
+#         unique_together = [("tipo_item",  "fase")]
+# #class TipoItem(models.Model):
+#  #   """"Esta clase representa a que tipo pertenece un item."""
+#   #  nombre = models.CharField(unique=True, max_length=50)
+#    # descripcion = models.CharField(max_length=50)
+#     #claves foraneas
+#     #fase = models.ForeignKey(Fase)
+#
+#     #def __unicode__(self):
+#      #   return self.nombre
+#
+# #class TipoItemFaseProyecto(models.Model):
+#  #   """Tabla que relaciona el tipo de item a una fase por proyecto."""
+#   #  proyecto = models.ForeignKey(Proyecto)
+#     #fase = models.ForeignKey(Fase)
+#    # tipo_item = models.ForeignKey(TipoItem)
+#     #cant = models.IntegerField(max_length = 4)
+#
+#     #def __unicode__(self):
+#      #   return self.tipo_item.descripcion
+#
+#     #class Meta:
+#      #   unique_together = [("tipo_item",  "proyecto")]
+#
+# class LineaBase(models.Model):
+#     #fecha_creacion = models.DateField(auto_now=False, auto_now_add=True, editable=False)
+#     #relaciones con otras tablas
+#     #proyectos = models.ForeignKey(Proyecto)
+#     #fase = models.ForeignKey(Fase)
+#      nombre = models.CharField(max_length=50)
+#     # items = models.IntegerField()
+#      #estado = modesl.CharField(max_length=50)
+#      estado = models.CharField(max_length=1, choices=LB_CHOICES)
+#      fase = models.ForeignKey(Fase)
+#
+#
+# class Item(models.Model):
+#     """Clase que representa a los items."""
+#     nombre = models.CharField(max_length=50)
+#     usuario = models.ForeignKey(User)
+#     fecha_creacion = models.DateField(auto_now_add=True, editable = False)
+#     estado = models.IntegerField(max_length=1, choices=STATUS_CHOICES, default=1)
+#     version = models.PositiveIntegerField()
+#     complejidad = models.IntegerField(max_length=1, choices=COMPLEXITY_CHOICES)
+#     descripcion_corta = models.TextField(null=True, blank=True)
+#     descripcion_larga = models.TextField(null=True, blank=True)
+#     habilitado = models.BooleanField(default=True)
+#     icono = models.FileField(upload_to='icono', null=True, blank=True)
+#     #claves foraneas
+#     proyecto = models.ForeignKey(Proyecto)
+#     tipo = models.ForeignKey(TipoItemFase)
+#     fase = models.ForeignKey(Fase)
+#     lbase= models.ForeignKey(LineaBase)
+#     def __unicode__(self):
+#         return self.nombre
+#
+# class RelItem(models.Model):
+#     padre = models.ForeignKey(Item, related_name = 'padre')
+#     hijo = models.ForeignKey(Item, related_name = 'hijo')
+#     habilitado = models.BooleanField(default=True)
+#
+#     class Meta:
+#         unique_together = [("padre", "hijo")]
+#
+# class Historial(models.Model):
+#     """Clase que representa el historial de los items"""
+#     usuario = models.ForeignKey(User)
+#     fecha_creacion = models.DateField(auto_now =False, auto_now_add=True, editable=False)
+#     #claves foraneas
+#     item = models.OneToOneField(Item, parent_link=False)
+#
+# class RegistroHistorial(models.Model):
+#     """Clase que representa el Registro de versiones de los items"""
+#     version = models.PositiveIntegerField()
+#     complejidad = models.IntegerField()
+#     descripcion_corta = models.TextField(null=True, blank=True)
+#     descripcion_larga = models.TextField(null=True, blank=True)
+#     habilitado = models.BooleanField()
+#     icono = models.FileField(upload_to='icono', null=True, blank=True)
+#     tipo = models.ForeignKey(TipoItemFase)
+#     fecha_modificacion = models.DateTimeField(auto_now=True, auto_now_add=False, editable=False)
+#     #claves foraneas
+#     historial = models.ForeignKey(Historial)
+#
+#
+# class Adjunto(models.Model):
+#     #archivo = models.FileField(upload_to='items')
+#     nombre = models.CharField(max_length = 100)
+#     contenido = models.TextField(null=True)
+#     tamano = models.IntegerField()
+#     mimetype = models.CharField(max_length = 255)
+#     #claves foraneas
+#     item = models.ForeignKey(Item)
+#     habilitado = models.BooleanField(default = True)
+# LB_CHOICES = ( ('R','REVISADO'),
+# 	       ('NR','NO REVISADO'),
+# 		('L','LIBERADO'),
+# 	)
+# #class LineaBase(models.Model):
+#     #fecha_creacion = models.DateField(auto_now=False, auto_now_add=True, editable=False)
+#     #relaciones con otras tablas
+#     #proyectos = models.ForeignKey(Proyecto)
+#     #fase = models.ForeignKey(Fase)
+#  #    nombre = models.CharField(max_length=50)
+#     # items = models.IntegerField()
+#      #estado = modesl.CharField(max_length=50)
+#   #   estado = models.CharField(max_length=1, choices=LB_CHOICES)
+#    #  fase = models.ForeignKey(Fase)
+# class UsuarioRolProyecto(models.Model):
+#     usuario = models.ForeignKey(User)
+#     rol = models.ForeignKey(Rol, null=True)
+#     proyecto = models.ForeignKey(Proyecto)
+#
+#     class Meta:
+#         unique_together = [("usuario", "rol", "proyecto")]
+#
+# class UsuarioRolSistema(models.Model):
+#     usuario = models.ForeignKey(User)
+#     rol = models.ForeignKey(Rol)
+#
+#     class Meta:
+#         unique_together = [("usuario", "rol")]
+#
+# class RegHistoRel(models.Model):
+#     itm_padre = models.ForeignKey(Item, related_name = 'itm_padre')
+#     itm_hijo = models.ForeignKey(Item, related_name = 'itm_hijo')
+#     registro = models.ForeignKey(RegistroHistorial)
+#
+#     class Meta:
+#         unique_together = [("itm_padre", "itm_hijo", "registro")]
+#
+# class RegHistoAdj(models.Model):
+#     #archivo = models.FileField(upload_to='items')
+#     nombre = models.CharField(max_length = 100)
+#     contenido = models.TextField(null=True)
+#     tamano = models.IntegerField()
+#     mimetype = models.CharField(max_length = 255)
+#     #claves foraneas
+#     item = models.ForeignKey(Item)
+#     #habilitado = models.BooleanField(default = True)
+#     registro = models.ForeignKey(RegistroHistorial)
+#
+#
+# class LineaBaseItems(models.Model):
+#     linea = models.ForeignKey(LineaBase)
+#     items = models.ForeignKey(Item)
+#     #procesos = models.CharField(max_length=1, choices=ACCIONES_CHOICES)
+#
+#     class Meta:
+#         unique_together = [("linea", "items")]
+#
+#
+# ESTADO_SOLICITUD = (
+#                  ('A', 'Aprobado'),
+#                  ('P', 'Pendiente'),
+# 		 ('R', 'Rechazado'),
+#              )
+#
+# class Comite(models.Model):
+# 	usuarios = models.ForeignKey(User)
+#         proyecto= models.ForeignKey(Proyecto)
+#
+# class Solicitud(models.Model):
+# 	usuario_solicitante = models.ForeignKey(User)
+# 	estado = models.CharField(max_length=1, choices=ESTADO_SOLICITUD)
+#
+# 	def __unicode__(self):
+# 		return self.usuario_solicitante
+# ACCIONES_CHOICES = ( ('M', 'Modificar'),
+# 		     ('B', 'Borrar'),)
+# class Acciones(models.Model):
+#     procesos = models.CharField(max_length=1, choices=ACCIONES_CHOICES)
+#     items = models.ForeignKey(Item)
+#     usu_solicitante = models.ForeignKey(User)
+#     proyecto =models.ForeignKey(Proyecto)
+#     aceptado = models.IntegerField(default=0)
+#     rechazado = models.IntegerField(default=0)
+#     votante= models.IntegerField()
+#     estado = models.CharField(max_length=1, choices=ESTADO_SOLICITUD)
