8d16b583c2b88e26d738203f54940bd0716c22d1
jorgemeza <jorgemeza93@gmail.com>
2015-03-26 23:17:41 -0400

    models User

--- SGPA/apps/usuario/tests.py
+++ SGPA/apps/usuario/tests.py
@@ -1,14 +1,16 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 import unittest
+import django
+django.setup()
 
 class UserTestCase(TestCase):
     def setUp(self):
-        User.objects.create(username="Jose")
+        User.objects.create(username="Carlos")
 
     def test_user_add(self):
-        user = User.objects.get(name="Jose")
-        self.assertEqual(user.get_username,'Jose')
+        user = User.objects.get(username="Carlos")
+        self.assertEqual(user.get_username(),"Carlos")
 
 if __name__ == "__main__":
-            unittest.main()
+    unittest.main()
